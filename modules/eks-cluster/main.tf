1d913b954dd018c0cbb28fdd352abe9feeea484c
Andrei Petrov <andrei.petrov@jetbrains.com>
2023-05-25 11:40:24 +0200

    fix(eks): remove the duplicated aws_caller_identity

--- modules/eks-cluster/main.tf
+++ modules/eks-cluster/main.tf
@@ -21,7 +21,6 @@ module "network" {
 
 // The below datas ources refer to a workaround with an issue with the default KMS key without resource-based policy:
 // associated with it. Ref: https://github.com/terraform-aws-modules/terraform-aws-eks/issues/2327#issuecomment-1355581682
-data "aws_caller_identity" "current" {}
 data "aws_iam_session_context" "current" {
   # "This data source provides information on the IAM source role of an STS assumed role. For non-role ARNs, this data source simply passes the ARN through in issuer_arn."
   arn = data.aws_caller_identity.current.arn
